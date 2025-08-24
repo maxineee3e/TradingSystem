ea36f2a19093aaaa98af357cd770fc2ed792e88e
Maxine <maxine@Maxines-MacBook-Air.local>
2025-08-24 15:16:16 +0800

    Integrate databento-cpp as submodule and update CMakeLists

---
+++ build/CMakeFiles/download_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/download_example.dir/src/main.cpp.o"
+  "CMakeFiles/download_example.dir/src/main.cpp.o.d"
+  "download_example"
+  "download_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/download_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/databento/CMakeFiles/databento.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/maxine/TradingSystem/databento/src/batch.cpp" "databento/CMakeFiles/databento.dir/src/batch.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/batch.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/datetime.cpp" "databento/CMakeFiles/databento.dir/src/datetime.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/datetime.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/dbn.cpp" "databento/CMakeFiles/databento.dir/src/dbn.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/dbn.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/dbn_decoder.cpp" "databento/CMakeFiles/databento.dir/src/dbn_decoder.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/dbn_decoder.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/dbn_encoder.cpp" "databento/CMakeFiles/databento.dir/src/dbn_encoder.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/dbn_encoder.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/dbn_file_store.cpp" "databento/CMakeFiles/databento.dir/src/dbn_file_store.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/dbn_file_store.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/detail/buffer.cpp" "databento/CMakeFiles/databento.dir/src/detail/buffer.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/detail/buffer.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/detail/dbn_buffer_decoder.cpp" "databento/CMakeFiles/databento.dir/src/detail/dbn_buffer_decoder.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/detail/dbn_buffer_decoder.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/detail/http_client.cpp" "databento/CMakeFiles/databento.dir/src/detail/http_client.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/detail/http_client.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/detail/json_helpers.cpp" "databento/CMakeFiles/databento.dir/src/detail/json_helpers.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/detail/json_helpers.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/detail/scoped_fd.cpp" "databento/CMakeFiles/databento.dir/src/detail/scoped_fd.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/detail/scoped_fd.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/detail/tcp_client.cpp" "databento/CMakeFiles/databento.dir/src/detail/tcp_client.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/detail/tcp_client.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/detail/zstd_stream.cpp" "databento/CMakeFiles/databento.dir/src/detail/zstd_stream.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/detail/zstd_stream.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/enums.cpp" "databento/CMakeFiles/databento.dir/src/enums.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/enums.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/exceptions.cpp" "databento/CMakeFiles/databento.dir/src/exceptions.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/exceptions.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/file_stream.cpp" "databento/CMakeFiles/databento.dir/src/file_stream.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/file_stream.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/flag_set.cpp" "databento/CMakeFiles/databento.dir/src/flag_set.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/flag_set.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/historical.cpp" "databento/CMakeFiles/databento.dir/src/historical.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/historical.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/live.cpp" "databento/CMakeFiles/databento.dir/src/live.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/live.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/live_blocking.cpp" "databento/CMakeFiles/databento.dir/src/live_blocking.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/live_blocking.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/live_threaded.cpp" "databento/CMakeFiles/databento.dir/src/live_threaded.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/live_threaded.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/log.cpp" "databento/CMakeFiles/databento.dir/src/log.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/log.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/metadata.cpp" "databento/CMakeFiles/databento.dir/src/metadata.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/metadata.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/pretty.cpp" "databento/CMakeFiles/databento.dir/src/pretty.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/pretty.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/publishers.cpp" "databento/CMakeFiles/databento.dir/src/publishers.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/publishers.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/record.cpp" "databento/CMakeFiles/databento.dir/src/record.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/record.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/symbol_map.cpp" "databento/CMakeFiles/databento.dir/src/symbol_map.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/symbol_map.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/symbology.cpp" "databento/CMakeFiles/databento.dir/src/symbology.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/symbology.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/v1.cpp" "databento/CMakeFiles/databento.dir/src/v1.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/v1.cpp.o.d"
+  "/Users/maxine/TradingSystem/databento/src/v2.cpp" "databento/CMakeFiles/databento.dir/src/v2.cpp.o" "gcc" "databento/CMakeFiles/databento.dir/src/v2.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
